a4976b36b47305a5a4d552cf3884a8842219afbe
Kurt Bonatz <dpole119@gmail.com>
2015-03-08 22:56:44 -0400

    Removed the fragment and am purely using an activity to make wikitude slightly easier to manage.

--- app/src/main/java/edu/gatech/cats/svreading/View/MainActivity.java
+++ app/src/main/java/edu/gatech/cats/svreading/View/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.gatech.cats.svreading.View;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
@@ -9,11 +8,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.wikitude.architect.ArchitectView;
 
 import edu.gatech.cats.svreading.R;
 
@@ -29,15 +31,33 @@ public class MainActivity extends ActionBarActivity {
      */
     private DrawerLayout drawerLayout;
 
+    /**
+     * Holds the Wikitude SDK ArchitectView (where the camera, markers, etc. are rendered)
+     */
+    private ArchitectView architectView;
+
+    private static final String WIKITUDE_SDK_KEY = "CSU+/8HIRGJh0bAqaYTpkp6qcnyfV5yEnOIFVEE/5T1N8k84kMJz8puHjhA+V+r7xGnizLw1CVt5/qYuvexwfmZeFwsOyuxetU5AC9F1s5/hzLKDNJOn0GBq8OhfpLnd3Ne3GO+b0ZOJDLmA75x/wWZXvmVtRBMblI94O2oBWmdTYWx0ZWRfX78Y6eenrSsW2xQDO7MkObj6INrJiZOQduDwZ0LtT5Ry85jU0FqBo+SkRvQOJYwYHzDBL00ca4zbDTGeqwgzDTG52hbWDCjvcZ6AH/BIZPxQUm4hNaRV6HRpLXs7AgWfEIpZ9W9r7o3oQq7gxGvkVhEkggE3XMEvFrWai9NXDgCHSeSx3MjVUxjIvHRj81ZsqEhrzq8jlDsO4525BIK+/zJOQfZc+M91apCtlrenLo7+Md1zK/+bgweB9pPXODj88aCA0anDzzUyurxPYMeLo7q6UDx90mLrxYW+AbL4F1PBTQAPaYiSyr+DUrqgGq5e832Bf5dIz/lhSNasE8rrUAe0hX0AbXq2fbk3ED8Ehz2YqCyiN+BW1RJGjbIl8ksYWhtTQ9z12Vkczwdd2dXBPk31B6EBHRFhpbVBiTLgyzxmv4HD9yRfdEGOjTSvCEECmb9yeAle9NnV1eH7i23O44kr30DHk6jBOGERaPT7vgrNMAxiGErTIBE=";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Set up the camera fragment
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.content_frame, new CameraFragment()).commit();
+        //Set the ArchitectView
+        this.architectView = (ArchitectView) findViewById(R.id.architectView);
+
+        //Pass the license key to the ArchitectView
+        final ArchitectView.ArchitectConfig config = new ArchitectView.ArchitectConfig(WIKITUDE_SDK_KEY);
+
+        //Try to create the ArchitectView
+        try{
+            this.architectView.onCreate(config);
+        } catch (RuntimeException e){
+            this.architectView = null;
+            Log.e(this.getClass().getName(), "Exception in ArchitectView.onCreate()", e);
+        }
 
         //Set up the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -71,6 +91,35 @@ public class MainActivity extends ActionBarActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         drawerToggle.syncState();
+
+        architectView.onPostCreate();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        //Resume the ArchitectView if possible
+        if(this.architectView != null){
+            this.architectView.onResume();
+        }
+    }
+
+    public void onPause(){
+        super.onPause();
+
+        //Pause the ArchitectView if possible
+        if(this.architectView != null){
+            this.architectView.onPause();
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(this.architectView != null){
+            this.architectView.onDestroy();
+        }
     }
 
     @Override
@@ -87,6 +136,14 @@ public class MainActivity extends ActionBarActivity {
             return;
         }
         super.onBackPressed();
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        if(this.architectView != null){
+            this.architectView.onLowMemory();
+        }
     }
 
     public void callYoutube(String url){
